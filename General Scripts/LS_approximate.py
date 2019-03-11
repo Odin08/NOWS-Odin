77e308757e7cdf6ad90ea346bee4d60092c68e69
Odin <40575647+Odin08@users.noreply.github.com>
2019-03-12 02:31:17 +0700

    Update LS_approximate.py

--- General Scripts/LS_approximate.py
+++ General Scripts/LS_approximate.py
@@ -5,13 +5,10 @@ from scipy import ndimage
 import numpy as np
 from numpy.fft import rfft, ifft
 from scipy.optimize import least_squares
-from scipy import integrate
+import scipy as sc
 import matplotlib.pyplot as plt
 from numpy import unravel_index
-import numba
 
-
-plt.nipy_spectral()
 
 pixel = 5.5e-3
 
@@ -22,75 +19,44 @@ def field(w0, x0, y0, X, Y):
     return I * np.exp( - ro / w0 ** 2)
 
 def intensity(p, arr):
-    X = np.arange(0, len(arr))
+    print(arr)
+    arr = arr.reshape(1024, 1024)
+    #x = np.arange(0, 1024)
+    #arr = (x[None, :]) ** 2 + (x[:, None]) ** 2
+    #E = field(60., 346., 490., 1024, 1024)
+    #E = E + 140
+    #arr = E
+    X = np.arange(0, arr.shape[0])
     w = p[0]
     I = p[1]
     x0 = p[2]
-    delta = p[3]
-    ro = (X - x0)**2
-    return I * np.exp( - ro / w ** 2) + delta
+    y0 = p[3]
+    delta = p[4]
+    ro = (X[:, None] - x0)**2 + (X[None, :] - y0)**2
+    Eth = I * np.exp( - ro / w ** 2) + delta
+    return np.abs(np.sum(Eth - arr))
 
 def wapp(arr):
-    x0 = arr.argmax()#unravel_index(arr.argmax(), arr.shape)
+    x0, y0 = unravel_index(arr.argmax(), arr.shape)
     X = np.arange(0, len(arr))
     I0 = arr.max()
-    res =  least_squares(lambda a, x, array: intensity(a, x) - array, [100., I0, x0, 1000], args=(X, arr))
+    arr1 = np.reshape(arr, 1024*1024)
+    #res =  sc.optimize.leastsq(intensity, [100., I0, x0, y0, 100], args=(arr1))
+    res = least_squares(intensity, [100., I0, x0, y0, 100], args=(arr1,))
     return res.x
-'''
-#@numba.jit
-def wapp(arr):
-    Deff_arr = []
-    Par = []
-    x0, y0 = unravel_index(arr.argmax(), arr.shape)
-    I0 = arr.max()
-    w0 = 10.
-    for i in np.arange(x0 - 20, x0 + 20):
-        print(i)
-        for j in np.arange(y0 - 20, y0 + 20):
-            for kI in np.arange(I0 - 50, I0 + 50):
-                for kw in np.arange(w0 - 10, w0 + 500):
-                    E = intensity([kw, kI, i, j], arr)
-                    Def = np.sum((E - arr)**2)
-                    print(Def)
-                    Deff_arr.append(Def)
-                    Par.append([kw, kI, i, j])
-    np.array(Deff_arr)
-    res = Par[Deff.argmin()]
-    print(res)
-    return res
-'''
 
-def load_bmg(file):
-    with open(file, 'rb') as f:
-        f.read(8)
-        s = f.read(8)
-        area = np.fromstring(s, dtype=np.int32)
-        f.read(20)
-        f.read(8)
-        f.read(41)
-        f.read(4)
-        f.read(358)
-        img = np.fromstring(f.read(area[0] * area[1] * 4), dtype=np.int).reshape(area[0], area[1])
-    #img = img[10:, :]
-    return img
-
-x = np.arange(0, 1024)
-arr = (x[None, :])**2 + (x[:, None])**2
 E = field(60., 346., 490., 1024, 1024)
-d = load_bmg('X:/gonta/CloudData-2019/first_test_m2/despecl_grinfilter_new/bl-30ms.bmg')
-E = E + d
-#plt.imshow(E)
-#plt.show()
+E = E + 140
+masx = wapp(E)
+print(masx)
+#print(intensity(masx))
+'''
+def fun_rosenbrock(x):
+    return np.array([1+ (x[1] - x[0]**2), (1 - x[0])])
 
-Ex = np.sum(E, axis=0)
-Ey = np.sum(E, axis=1)
-masx = wapp(Ex)
-masy = wapp(Ey)
-plt.plot(Ex)
-plt.plot(Ey)
-plt.plot(intensity(masx, np.arange(0, len(Ex))))
-plt.plot(intensity(masy, np.arange(0, len(Ey))))
-plt.show()
+from scipy.optimize import least_squares
+x0_rosenbrock = np.array([20, 20])
+res_1 = least_squares(fun_rosenbrock, x0_rosenbrock)
+print(res_1.x)
 
-
-
+'''
