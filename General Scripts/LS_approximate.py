3cef5cc82838ca63ddc7a6cc9669b09fd8393a6a
Odin <40575647+Odin08@users.noreply.github.com>
2019-03-12 03:05:12 +0700

    Update LS_approximate.py

--- General Scripts/LS_approximate.py
+++ General Scripts/LS_approximate.py
@@ -9,54 +9,42 @@ import scipy as sc
 import matplotlib.pyplot as plt
 from numpy import unravel_index
 
+plt.nipy_spectral()
 
 pixel = 5.5e-3
 
-def field(w0, x0, y0, X, Y):
+def field(w0, I, x0, y0, X, Y):
     X, Y = np.mgrid[0:X, 0:Y]
     ro = (X - x0)**2 + (Y - y0)**2
-    I = 3500
     return I * np.exp( - ro / w0 ** 2)
 
 def intensity(p, arr):
-    print(arr)
-    arr = arr.reshape(1024, 1024)
-    #x = np.arange(0, 1024)
-    #arr = (x[None, :]) ** 2 + (x[:, None]) ** 2
-    #E = field(60., 346., 490., 1024, 1024)
-    #E = E + 140
-    #arr = E
-    X = np.arange(0, arr.shape[0])
+    X, Y = np.mgrid[0:arr.shape[0], 0:arr.shape[1]]
     w = p[0]
     I = p[1]
     x0 = p[2]
     y0 = p[3]
     delta = p[4]
-    ro = (X[:, None] - x0)**2 + (X[None, :] - y0)**2
+    ro = (X - x0)**2 + (Y - y0)**2
     Eth = I * np.exp( - ro / w ** 2) + delta
-    return np.abs(np.sum(Eth - arr))
+    l = Eth - arr
+    l = np.reshape(l, arr.shape[0]*arr.shape[1])
+    return l
 
 def wapp(arr):
     x0, y0 = unravel_index(arr.argmax(), arr.shape)
-    X = np.arange(0, len(arr))
     I0 = arr.max()
-    arr1 = np.reshape(arr, 1024*1024)
-    #res =  sc.optimize.leastsq(intensity, [100., I0, x0, y0, 100], args=(arr1))
-    res = least_squares(intensity, [100., I0, x0, y0, 100], args=(arr1,))
+    res = least_squares(intensity, [100., I0, x0, y0, 100.], args=(arr,))
     return res.x
 
-E = field(60., 346., 490., 1024, 1024)
-E = E + 140
+E = field(60., 3500., 346., 490., 1024, 1024)
+E = E + 140.
+d = 200*np.random.rand(1024, 1024)
+E = E + d
 masx = wapp(E)
 print(masx)
-#print(intensity(masx))
-'''
-def fun_rosenbrock(x):
-    return np.array([1+ (x[1] - x[0]**2), (1 - x[0])])
-
-from scipy.optimize import least_squares
-x0_rosenbrock = np.array([20, 20])
-res_1 = least_squares(fun_rosenbrock, x0_rosenbrock)
-print(res_1.x)
-
-'''
+print(intensity(masx, E))
+#plt.plot(E.sum(axis=0))
+#plt.plot((field(masx[0], masx[1], masx[2], masx[3], 1024, 1024)+ masx[4]).sum(axis=0))
+plt.imshow(E)
+plt.show()
